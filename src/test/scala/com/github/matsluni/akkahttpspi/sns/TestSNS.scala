8e80654da1b196a4fa7e2ef305cb5226f8abbe38
João Ferreira <joao@c02d174smd6r.tail3da01.ts.net>
2023-10-19 17:28:33 +0100

    add test

--- src/test/scala/com/github/matsluni/akkahttpspi/sns/TestSNS.scala
+++ src/test/scala/com/github/matsluni/akkahttpspi/sns/TestSNS.scala
@@ -16,10 +16,18 @@
 
 package com.github.matsluni.akkahttpspi.sns
 
+import com.github.dockerjava.api.model.Frame
 import com.github.matsluni.akkahttpspi.{AkkaHttpAsyncHttpService, LocalstackBaseAwsClientTest}
+import org.testcontainers.shaded.com.github.dockerjava.core.InvocationBuilder.AsyncResultCallback
 import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, StaticCredentialsProvider}
-import software.amazon.awssdk.services.sns.SnsAsyncClient
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration
+import software.amazon.awssdk.core.retry.RetryPolicy
+import software.amazon.awssdk.core.retry.backoff.FixedDelayBackoffStrategy
 import software.amazon.awssdk.services.sns.model.{CreateTopicRequest, PublishRequest}
+import software.amazon.awssdk.services.sns.{SnsAsyncClient, SnsAsyncClientBuilder}
+
+import java.time.Duration
+import scala.concurrent.duration.DurationInt
 
 class TestSNS extends LocalstackBaseAwsClientTest[SnsAsyncClient] {
 
@@ -32,17 +40,36 @@ class TestSNS extends LocalstackBaseAwsClientTest[SnsAsyncClient] {
     }
   }
 
-  def withClient(testCode: SnsAsyncClient => Any): Any = {
+  "Retryable SNS client" should {
+    "retry" in withLongRetriesClient { client =>
+      //the localstack process wil take a few seconds to restart
+      killLocalstackProcess()
+
+      val startTime = System.currentTimeMillis()
+      val listTopicsFuture = client.listTopics()
+
+      //If debug logs on "software.amazon" are enabled, one should see the retry messages
+      //"Retryable error detected. Will retry in 1000ms. Request attempt number 2"
+      listTopicsFuture.join()
+
+      val duration = System.currentTimeMillis() - startTime
+      duration shouldBe > (3.seconds.toMillis)
+    }
+  }
+
+  def withClient(testCode: SnsAsyncClient => Any): Any = withCustomClient(identity)(testCode)
+  def withCustomClient(builderFn: SnsAsyncClientBuilder => SnsAsyncClientBuilder)(testCode: SnsAsyncClient => Any): Any = {
 
     val akkaClient = new AkkaHttpAsyncHttpService().createAsyncHttpClientFactory().build()
 
-    val client = SnsAsyncClient
+    val builder = SnsAsyncClient
       .builder()
       .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("x", "x")))
       .httpClient(akkaClient)
       .region(defaultRegion)
       .endpointOverride(endpoint)
-      .build()
+
+    val client = builderFn(builder).build()
 
     try {
       testCode(client)
@@ -52,6 +79,17 @@ class TestSNS extends LocalstackBaseAwsClientTest[SnsAsyncClient] {
       client.close()
     }
   }
+
+  /** Uses an SnsAsyncClient will retry more times than the default retry policy (3 retries) and with a bigger delay between retries */
+  private def withLongRetriesClient(testCode: SnsAsyncClient => Any) = withCustomClient(b =>
+    b.overrideConfiguration { (b: ClientOverrideConfiguration.Builder) =>
+      b.retryPolicy(RetryPolicy.builder()
+        .numRetries(6)
+        .backoffStrategy(FixedDelayBackoffStrategy.create(Duration.ofSeconds(1)))
+        .build()
+      )
+    }
+  )(testCode)
 
   override def service: String = "sns"
 }
